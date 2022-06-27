c3031161da624f33e0f2ee0585dec834bfcfbde1
Keltaisten <keltaisten@gmail.com>
2022-06-27 16:04:54 +0200

    init

---
+++ Beer-Webshop-Project/src/test/java/beerwebshopproject/BeerWebshopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package beerwebshopproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerWebshopProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
